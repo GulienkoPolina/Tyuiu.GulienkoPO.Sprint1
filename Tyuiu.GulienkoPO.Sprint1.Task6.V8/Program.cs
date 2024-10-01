e70b80f840bd14618cd4f7cc16e796889ec5303e
Polina <gulienko.polina@mail.ru>
2024-10-01 21:25:47 +0500

    Task6

---
+++ Tyuiu.GulienkoPO.Sprint1.Task6.V8/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.GulienkoPO.Sprint1.Task6.V8.Lib;
+
+namespace Tyuiu.GulienkoPO.Sprint1.Task6.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Гулиенко П. О. | ИСТНб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнила: Гулиенко Полина Олеговна | ИСТНб-24-1                       .*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Напечатать все слова,    *");
+            Console.WriteLine("* перенеся их первую букву в конец.                                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.MoveLetterToEnd(str));
+
+            Console.ReadKey();
+        }
+    }
+}
