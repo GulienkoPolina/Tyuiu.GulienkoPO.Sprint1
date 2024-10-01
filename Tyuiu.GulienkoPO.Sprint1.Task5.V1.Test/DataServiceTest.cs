f2cb41ac53298fb4d35d4a5b3e13272902c34ed0
Polina <gulienko.polina@mail.ru>
2024-10-01 21:08:09 +0500

    Task5

---
+++ Tyuiu.GulienkoPO.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+using Tyuiu.GulienkoPO.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.GulienkoPO.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x1 = 5;
+            double y1 = 3;
+            double x2 = 2;
+            double y2 = 1;
+            DataService ds = new DataService();
+            double res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 3;
+
+            Assert.AreEqual(result, wait);
+
+        }
+    }
+}
